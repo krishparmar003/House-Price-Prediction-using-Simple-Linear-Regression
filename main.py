468e7ec3e98f171c24970a1bc26e1c1533e46355
Krish Parmar <krishparmar003@gmail.com>
2026-03-22 20:38:44 +0530

    updated main.py

--- main.py
+++ main.py
@@ -5,4 +5,18 @@ import pandas as pd
 
 def generate_house_data(n_samples=100):
     np.random.seed(50)
+    size = np.random.normal(1400, 50, n_samples)
+    price = size * 50 + np.random.normal(0, 50, n_samples)
+    return pd.DataFrame({'size': size, 'price': price})
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.linear_model import LinearRegression
+
+def train_model():
+    df = generate_house_data(n_samples=100)
+    X = df[['size']]
+    Y = df['price']
+    X_train, Y_train, X_test, Y_test = train_test_split(X, Y, test_size=0.2)
+
     
